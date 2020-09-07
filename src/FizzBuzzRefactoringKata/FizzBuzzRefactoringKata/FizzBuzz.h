44cf2e392f1278c0287fe33393cabb5015cf359a
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-07 17:02:02 +0200

    remove 15 as special case but handle it as 3 and 5 result composition

--- src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzz.h
+++ src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzz.h
@@ -6,25 +6,20 @@ public:
 	static std::string say(const int number)
 	{
 		std::string value = "";
-		if (number % 15 == 0)
-		{
-			auto str = "FizzBuzz";
-			value += str;
-		}
-		else if (number % 5 == 0)
-		{
-			auto str = "Buzz";
-			value += str;
-		}
-		else if (number % 3 == 0)
+		if (number % 3 == 0)
 		{
 			auto str = "Fizz";
+			value += str;
+		}
+		if (number % 5 == 0)
+		{
+			auto str = "Buzz";
 			value += str;
 		}
 
 		if (std::empty(value))
 			return std::to_string(number);
-		else 
+		else
 			return value;
 	}
 };
