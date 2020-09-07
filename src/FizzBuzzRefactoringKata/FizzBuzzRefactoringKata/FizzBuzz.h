b953f33cf95988b0a8c5191ce466fd9905e24ea6
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-07 17:05:07 +0200

    Bang! green bar :-)

--- src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzz.h
+++ src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzz.h
@@ -8,6 +8,7 @@ public:
 		std::string value = "";
 		if (number % 3 == 0) value += "Fizz";
 		if (number % 5 == 0) value += "Buzz";
+		if (number % 7 == 0) value += "Bang";
 
 		return std::empty(value) ? std::to_string(number) : value;
 	}
