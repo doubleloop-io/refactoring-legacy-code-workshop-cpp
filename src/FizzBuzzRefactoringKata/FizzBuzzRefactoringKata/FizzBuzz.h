b487ecd801c3871638d044797a9c2b755d8ee1ea
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-07 16:08:35 +0200

    initial FizzBuzz implementation

---
+++ src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzz.h
@@ -0,0 +1,13 @@
+﻿#include <string>
+
+class FizzBuzz
+{
+public:
+	static std::string say(const int number)
+	{
+		if (number % 15 == 0) return "FizzBuzz";
+		if (number % 5 == 0)  return "Buzz";
+		if (number % 3 == 0)  return "Fizz";
+		return std::to_string(number);
+	}
+};
---
+++ src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzzTest.cpp
@@ -0,0 +1,32 @@
+#include "gtest/gtest.h"
+#include "FizzBuzz.h"
+
+using namespace std;
+
+class FizzBuzzerTestFixture :public ::testing::TestWithParam<tuple<int, string>> { };
+
+TEST_P(FizzBuzzerTestFixture, SingleNumber) {
+	int number = std::get<0>(GetParam());
+	const string expected = std::get<1>(GetParam());
+	ASSERT_EQ(FizzBuzz::say(number), expected);
+}
+
+INSTANTIATE_TEST_CASE_P(
+	FizzBuzzerTests, // -- random name
+	FizzBuzzerTestFixture, // -- fixture name
+	::testing::Values(
+		make_tuple(3, "Fizz"),
+		make_tuple(3 * 2, "Fizz"),
+		// --
+		make_tuple(5, "Buzz"),
+		make_tuple(5 * 2, "Buzz"),
+		// --
+		make_tuple(15, "FizzBuzz"),
+		make_tuple(15 * 2, "FizzBuzz"),
+		// --
+		make_tuple(1, "1"),
+		make_tuple(2, "2"),
+		make_tuple(4, "4"),
+		make_tuple(8, "8")
+	) // -- input data
+);
