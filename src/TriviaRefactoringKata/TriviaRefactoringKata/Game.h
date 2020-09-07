d958571d392cef57309cad056661ed1f74f126c5
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-07 16:18:20 +0200

    initial Trivia implementation

---
+++ src/TriviaRefactoringKata/TriviaRefactoringKata/Game.h
@@ -0,0 +1,46 @@
+﻿#pragma once 
+
+#include <iostream>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+class Game {
+
+private:
+	vector<string> players;
+
+	int places[6];
+	int purses[6];
+
+	bool inPenaltyBox[6];
+
+	list<string> popQuestions;
+	list<string> scienceQuestions;
+	list<string> sportsQuestions;
+	list<string> rockQuestions;
+
+	unsigned int currentPlayer;
+	bool isGettingOutOfPenaltyBox;
+
+public:
+	Game();
+	string createRockQuestion(int index);
+	bool isPlayable();
+	bool add(string playerName);
+
+	int howManyPlayers();
+	void roll(int roll);
+
+private:
+	void askQuestion();
+	string currentCategory();
+
+public:
+	bool wasCorrectlyAnswered();
+	bool wrongAnswer();
+
+private:
+	bool didPlayerWin();
+};
