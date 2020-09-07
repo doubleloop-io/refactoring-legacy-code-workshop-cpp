86b143d7188fcacdec26e0f8b37b4ce1eaf4ad3a
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-07 16:56:32 +0200

    extract local variables

--- src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzz.h
+++ src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzz.h
@@ -5,9 +5,21 @@ class FizzBuzz
 public:
 	static std::string say(const int number)
 	{
-		if (number % 15 == 0) return "FizzBuzz";
-		if (number % 5 == 0)  return "Buzz";
-		if (number % 3 == 0)  return "Fizz";
+		if (number % 15 == 0)
+		{
+			auto str = "FizzBuzz";
+			return str;
+		}
+		if (number % 5 == 0)
+		{
+			auto str = "Buzz";
+			return str;
+		}
+		if (number % 3 == 0)
+		{
+			auto str = "Fizz";
+			return str;
+		}
 		return std::to_string(number);
 	}
 };
