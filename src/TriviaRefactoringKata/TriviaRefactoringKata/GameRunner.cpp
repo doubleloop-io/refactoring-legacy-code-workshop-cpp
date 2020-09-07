78d4354c74e2ed02dcbfcbbc192f9780a3fa44f4
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-07 18:51:05 +0200

    extract main logic into GameRunner class

---
+++ src/TriviaRefactoringKata/TriviaRefactoringKata/GameRunner.cpp
@@ -0,0 +1,33 @@
+﻿#include "GameRunner.h"
+#include <cstdlib>
+#include <ctime>
+#include "Game.h"
+
+bool GameRunner::notAWinner;
+
+void GameRunner::run(int argc, char* argv[])
+{
+	srand((int)time(NULL));
+
+	Game aGame;
+
+	aGame.add("Chet");
+	aGame.add("Pat");
+	aGame.add("Sue");
+
+	do
+	{
+
+		aGame.roll(rand() % 5 + 1);
+
+		if (rand() % 9 == 7)
+		{
+			notAWinner = aGame.wrongAnswer();
+		}
+		else
+		{
+			notAWinner = aGame.wasCorrectlyAnswered();
+		}
+	} while (notAWinner);
+}
+
