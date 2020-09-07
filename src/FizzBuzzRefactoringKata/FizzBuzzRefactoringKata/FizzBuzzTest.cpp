3131cfeab579c416bea8173648c36cbd6460260c
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-07 17:04:34 +0200

    re-introduce failing 7 cases

--- src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzzTest.cpp
+++ src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzzTest.cpp
@@ -25,9 +25,11 @@ INSTANTIATE_TEST_CASE_P(
 		make_tuple(15, "FizzBuzz"),
 		make_tuple(15 * 2, "FizzBuzz"),
 		// --
-		// make_tuple(7, "Bang"),
-		// make_tuple(7 * 2, "Bang"),
-		// make_tuple(7 * 3, "FizzBang"),
+		make_tuple(7, "Bang"),
+		make_tuple(7 * 2, "Bang"),
+		make_tuple(7 * 3, "FizzBang"),
+		make_tuple(7 * 5, "BuzzBang"),
+		make_tuple(7 * 5 * 3, "FizzBuzzBang"),
 		// --
 		make_tuple(1, "1"),
 		make_tuple(2, "2"),
