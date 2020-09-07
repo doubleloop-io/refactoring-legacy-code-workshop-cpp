8a39d529208887e5a5d2778d7597ac88a1f675c7
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-07 17:03:40 +0200

    post refactoring cleanup

--- src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzz.h
+++ src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzz.h
@@ -6,20 +6,9 @@ public:
 	static std::string say(const int number)
 	{
 		std::string value = "";
-		if (number % 3 == 0)
-		{
-			auto str = "Fizz";
-			value += str;
-		}
-		if (number % 5 == 0)
-		{
-			auto str = "Buzz";
-			value += str;
-		}
+		if (number % 3 == 0) value += "Fizz";
+		if (number % 5 == 0) value += "Buzz";
 
-		if (std::empty(value))
-			return std::to_string(number);
-		else
-			return value;
+		return std::empty(value) ? std::to_string(number) : value;
 	}
 };
