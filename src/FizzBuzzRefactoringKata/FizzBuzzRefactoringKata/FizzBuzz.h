c6fd7b8e7ed5824c7882f8c27a02a440ed0b992c
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-07 16:57:19 +0200

    add shared function variable

--- src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzz.h
+++ src/FizzBuzzRefactoringKata/FizzBuzzRefactoringKata/FizzBuzz.h
@@ -5,19 +5,23 @@ class FizzBuzz
 public:
 	static std::string say(const int number)
 	{
+		std::string value = "";
 		if (number % 15 == 0)
 		{
 			auto str = "FizzBuzz";
+			value = str;
 			return str;
 		}
 		if (number % 5 == 0)
 		{
 			auto str = "Buzz";
+			value = str;
 			return str;
 		}
 		if (number % 3 == 0)
 		{
 			auto str = "Fizz";
+			value = str;
 			return str;
 		}
 		return std::to_string(number);
