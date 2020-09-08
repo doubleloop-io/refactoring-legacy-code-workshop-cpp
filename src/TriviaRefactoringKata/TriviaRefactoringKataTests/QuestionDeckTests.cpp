f17677bbe9879876866b605a6c2ea1a1bfbc5aca
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-08 11:36:32 +0200

    remove old redundant tests

--- src/TriviaRefactoringKata/TriviaRefactoringKataTests/QuestionDeckTests.cpp
+++ src/TriviaRefactoringKata/TriviaRefactoringKataTests/QuestionDeckTests.cpp
@@ -23,15 +23,3 @@ INSTANTIATE_TEST_CASE_P(
 		make_pair(8, "Pop")
 	)
 );
-
-TEST(QuestionDeckTests, PopCategoryForPlaceZero) {
-	QuestionDeck deck;
-	auto category = deck.current_category(0);
-	ASSERT_EQ(category, "Pop");
-}
-
-TEST(QuestionDeckTests, PopCategoryForPlaceFour) {
-	QuestionDeck deck;
-	auto category = deck.current_category(4);
-	ASSERT_EQ(category, "Pop");
-}
