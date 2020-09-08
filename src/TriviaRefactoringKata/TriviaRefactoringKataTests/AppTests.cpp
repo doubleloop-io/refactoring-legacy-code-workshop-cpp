991dd7f03656d88ef89c956b176eebcb5ffb74f9
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-08 08:18:26 +0200

    prepare a TestableGameRunner

--- src/TriviaRefactoringKata/TriviaRefactoringKataTests/AppTests.cpp
+++ src/TriviaRefactoringKata/TriviaRefactoringKataTests/AppTests.cpp
@@ -5,10 +5,15 @@
 
 namespace fs = std::experimental::filesystem;
 
+class TestableGameRunner : public GameRunner
+{
+
+};
+
 TEST(AppTest, Run)
 {
 	auto wd = fs::current_path().append("outputs").append("app_output.txt");
 	FileRedirect fr(wd.string());
-	GameRunner runner;
+	TestableGameRunner runner;
 	runner.run(0, nullptr);
 }
