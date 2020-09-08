cba58510cb96c341e92e67f06eb2fde39c532972
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-08 08:58:31 +0200

    add characterization test with golden master

--- src/TriviaRefactoringKata/TriviaRefactoringKataTests/AppTests.cpp
+++ src/TriviaRefactoringKata/TriviaRefactoringKataTests/AppTests.cpp
@@ -5,11 +5,24 @@
 
 namespace fs = std::experimental::filesystem;
 
-TEST(AppTest, Run)
-{
-	auto wd = fs::current_path().append("outputs").append("app_output.txt");
-	FileRedirect fr(wd.string());
+auto run_output_path = fs::current_path().append("outputs").append("app_output.txt");
+auto golden_master_output_path = fs::current_path().append("app_output.golden_master.txt");
 
+std::string read_content(fs::path path)
+{
+	std::string line, content;
+	std::ifstream file(path);
+	if (!file.is_open()) return "";
+
+	while (std::getline(file, line))
+	{
+		content += line + '\n';
+	}
+	return content;
+}
+
+void game_runs_for(int count)
+{
 	for (size_t i = 0; i < 1000; i++)
 	{
 		// seed distribution 
@@ -18,4 +31,17 @@ TEST(AppTest, Run)
 		auto seed = 3645 * i + 42;
 		GameRunner::run(seed);
 	}
+}
+
+TEST(AppTest, GoldenMaster)
+{
+	{
+		FileRedirect fr(run_output_path.string());
+		game_runs_for(1000);
+	}
+
+	auto actual = read_content(run_output_path);
+	auto expected = read_content(golden_master_output_path);
+
+	ASSERT_EQ(actual, expected);
 }
