1f4f7996994df485dd14d62f7a51103a46e6f0c9
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-08 11:35:57 +0200

    refactor to parametric tests

--- src/TriviaRefactoringKata/TriviaRefactoringKataTests/QuestionDeckTests.cpp
+++ src/TriviaRefactoringKata/TriviaRefactoringKataTests/QuestionDeckTests.cpp
@@ -1,6 +1,29 @@
 ﻿#include "gtest/gtest.h"
 #include "../TriviaRefactoringKata/QuestionDeck.h"
 
+using namespace std;
+
+class QuestionDeckTestsFixture : public ::testing::TestWithParam<tuple<int, string>> {};
+
+TEST_P(QuestionDeckTestsFixture, LookupCategoryByPlace) {
+	int place = get<0>(GetParam());
+	string expected = get<1>(GetParam());
+
+	QuestionDeck deck;
+	auto category = deck.current_category(place);
+	ASSERT_EQ(category, expected);
+}
+
+INSTANTIATE_TEST_CASE_P(
+	QuestionDeckTests,
+	QuestionDeckTestsFixture,
+	::testing::Values(
+		make_pair(0, "Pop"),
+		make_pair(4, "Pop"),
+		make_pair(8, "Pop")
+	)
+);
+
 TEST(QuestionDeckTests, PopCategoryForPlaceZero) {
 	QuestionDeck deck;
 	auto category = deck.current_category(0);
