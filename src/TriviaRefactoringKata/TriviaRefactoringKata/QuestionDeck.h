3054fd584368cd97697a84e64b3ab3bdefe7d51d
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-08 11:24:50 +0200

    close QuestionDeck's members visibility

--- src/TriviaRefactoringKata/TriviaRefactoringKata/QuestionDeck.h
+++ src/TriviaRefactoringKata/TriviaRefactoringKata/QuestionDeck.h
@@ -6,13 +6,15 @@ using namespace std;
 class QuestionDeck
 {
 public:
+	void fill_question_deck();
+	string current_category(int current_place);
+	void ask_question(string current_category);
+
+private:
 	list<string> popQuestions_;
 	list<string> scienceQuestions_;
 	list<string> sportsQuestions_;
 	list<string> rockQuestions_;
 
 	string createRockQuestion(int index);
-	void fill_question_deck();
-	string current_category(int current_place);
-	void ask_question(string current_category);
 };
