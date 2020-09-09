dd3e58c3790714be7891dc9033ee23f97507d067
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-09 12:10:24 +0200

    use GameRunner inside main

--- src/TriviaRefactoringKata/TriviaRefactoringKata/Main.cpp
+++ src/TriviaRefactoringKata/TriviaRefactoringKata/Main.cpp
@@ -1,30 +1,6 @@
-﻿#include "Game.h"
-#include <ctime>
-
-static bool notAWinner;
+﻿#include "GameRunner.h"
 
 int main(int argc, char *argv[])
 {
-	srand((int)time(NULL));
-
-	Game aGame;
-
-	aGame.add("Chet");
-	aGame.add("Pat");
-	aGame.add("Sue");
-
-	do
-	{
-
-		aGame.roll(rand() % 5 + 1);
-
-		if (rand() % 9 == 7)
-		{
-			notAWinner = aGame.wrongAnswer();
-		}
-		else
-		{
-			notAWinner = aGame.wasCorrectlyAnswered();
-		}
-	} while (notAWinner);
+	GameRunner::run(argc, argv);
 }
