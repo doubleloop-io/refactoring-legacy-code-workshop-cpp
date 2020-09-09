2b2227bf96ebb14209242a62e980e892fba3b198
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-09 14:37:54 +0200

    implement FileRedirect in order to restore original cout

--- src/TriviaRefactoringKata/TriviaRefactoringKataTests/test.cpp
+++ src/TriviaRefactoringKata/TriviaRefactoringKataTests/test.cpp
@@ -4,8 +4,8 @@
 #include <experimental/filesystem>
 
 TEST(DemoTest, SimpleAssertions) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
 }
 
 class TestableGameRunner : public GameRunner
@@ -26,13 +26,31 @@ private:
 	const int seed_;
 };
 
+class FileRedirect
+{
+public:
+	FileRedirect(std::string path)
+	{
+		output_file_.open(path);
+		original_ = std::cout.rdbuf(output_file_.rdbuf());
+	}
+
+	~FileRedirect()
+	{
+		std::cout.rdbuf(original_);
+		output_file_.close();
+	}
+
+private:
+	std::ofstream output_file_;
+	std::streambuf* original_;
+};
+
+auto app_out_path = std::experimental::filesystem::current_path().append("app_output.txt");
 
 TEST(ReplTest, GameRun)
 {
-	auto p2 = std::experimental::filesystem::current_path().append("app_output.txt");
-	std::ofstream os(p2);
-	std::cout.rdbuf(os.rdbuf());
-
+	FileRedirect redirect(app_out_path.string());
 	TestableGameRunner runner(10);
 	runner.run(0, nullptr);
 }
