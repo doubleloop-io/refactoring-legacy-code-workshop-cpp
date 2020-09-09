dd492455686e4196c94f98e494193d2c3cece64b
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-09 12:17:47 +0200

    execute GameRunner::run from test suite

--- src/TriviaRefactoringKata/TriviaRefactoringKataTests/test.cpp
+++ src/TriviaRefactoringKata/TriviaRefactoringKataTests/test.cpp
@@ -1,6 +1,13 @@
 #include "gtest/gtest.h"
+#include "../TriviaRefactoringKata/GameRunner.h"
 
 TEST(DemoTest, SimpleAssertions) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+
+TEST(ReplTest, GameRun)
+{
+	GameRunner::run(0, nullptr);
 }
