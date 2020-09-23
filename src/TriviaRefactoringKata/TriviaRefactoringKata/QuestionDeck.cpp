a575d95699e0da263db8df33efa2403c7d697b34
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-23 11:28:16 +0200

    clean fill_questions

--- src/TriviaRefactoringKata/TriviaRefactoringKata/QuestionDeck.cpp
+++ src/TriviaRefactoringKata/TriviaRefactoringKata/QuestionDeck.cpp
@@ -2,31 +2,21 @@
 #include <sstream>
 #include <iostream>
 
-static std::string create_rock_question(int index)
+static std::string create_question(int index, std::string category)
 {
-	char indexStr[127];
-	sprintf_s(indexStr, "Rock Question %d", index);
-	return indexStr;
+	std::ostringstream oss(std::ostringstream::out);
+	oss << category + " Question " << index;
+	return oss.str();
 }
 
 void QuestionDeck::fill_questions()
 {
 	for (int i = 0; i < 50; i++)
 	{
-		std::ostringstream oss(std::ostringstream::out);
-		oss << "Pop Question " << i;
-
-		popQuestions_.push_back(oss.str());
-
-		char str[255];
-		sprintf_s(str, "Science Question %d", i);
-		scienceQuestions_.push_back(str);
-
-		char str1[255];
-		sprintf_s(str1, "Sports Question %d", i);
-		sportsQuestions_.push_back(str1);
-
-		rockQuestions_.push_back(create_rock_question(i));
+		popQuestions_.push_back(create_question(i, "Pop"));
+		scienceQuestions_.push_back(create_question(i, "Science"));
+		sportsQuestions_.push_back(create_question(i, "Sports"));
+		rockQuestions_.push_back(create_question(i, "Rock"));
 	}
 }
 
