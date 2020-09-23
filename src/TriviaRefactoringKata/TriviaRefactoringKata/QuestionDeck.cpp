cfd0b48d512f0d4b8f2b0921dbf437d75ff729e0
Matteo Baglini <matteo.baglini@gmail.com>
2020-09-23 16:20:22 +0200

    replace single category fields with a map

--- src/TriviaRefactoringKata/TriviaRefactoringKata/QuestionDeck.cpp
+++ src/TriviaRefactoringKata/TriviaRefactoringKata/QuestionDeck.cpp
@@ -1,7 +1,5 @@
 ﻿#include "QuestionDeck.h"
 #include <sstream>
-#include <iostream>
-#include <unordered_set>
 
 static std::string create_question(int index, std::string category)
 {
@@ -10,48 +8,32 @@ static std::string create_question(int index, std::string category)
 	return oss.str();
 }
 
-static bool places_contains(const std::unordered_set<int>& places, int value)
-{
-	return places.find(value) != places.end();
-}
-
-static std::string next_question(std::list<std::string>& questions)
-{
-	auto question = questions.front();
-	questions.pop_front();
-	return question;
-}
-
 void QuestionDeck::fill_questions()
 {
-	for (int i = 0; i < 50; i++)
+	for (auto& pair : categories_)
 	{
-		pop_.append_question(create_question(i, pop_.name()));
-
-		scienceQuestions_.push_back(create_question(i, "Science"));
-		sportsQuestions_.push_back(create_question(i, "Sports"));
-		rockQuestions_.push_back(create_question(i, "Rock"));
+		for (int i = 0; i < 50; i++)
+		{
+			pair.second.append_question(create_question(i, pair.first));
+		}
 	}
 }
 
 std::string QuestionDeck::find_category_for(int place) const
 {
-	if (pop_.places_contains(place)) return pop_.name();
-
-	if (places_contains(sciencePlaces_, place)) return "Science";
-	if (places_contains(sportsPlaces_, place)) return "Sports";
-	if (places_contains(rockPlaces_, place)) return "Rock";
-
+	for (auto& pair : categories_)
+	{
+		if (pair.second.places_contains(place))
+			return pair.first;
+	}
 	throw PlaceNotFoundException(place);
 }
 
 std::string QuestionDeck::fetch_next_question(std::string category)
 {
-	if (category == pop_.name()) return pop_.next_question();
-
-	if (category == "Science") return next_question(scienceQuestions_);
-	if (category == "Sports") return next_question(sportsQuestions_);
-	if (category == "Rock") return next_question(rockQuestions_);
-
+	auto it = categories_.find(category);
+	if (it != categories_.end())
+		return it->second.next_question();
 	return "";
 }
+
--- src/TriviaRefactoringKata/TriviaRefactoringKata/QuestionDeck.h
+++ src/TriviaRefactoringKata/TriviaRefactoringKata/QuestionDeck.h
@@ -1,8 +1,8 @@
 ﻿#pragma once
-#include <list>
 #include <unordered_set>
 #include <string>
 #include "QuestionCategory.h"
+#include <unordered_map>
 
 class QuestionDeck
 {
@@ -12,16 +12,12 @@ public:
 	std::string fetch_next_question(std::string category);
 
 private:
-	QuestionCategory pop_ = QuestionCategory("Pop", {0,4,8});
-
-	std::list<std::string> scienceQuestions_;
-	const std::unordered_set<int> sciencePlaces_ = { 1,5,9 };
-
-	std::list<std::string> sportsQuestions_;
-	const std::unordered_set<int> sportsPlaces_ = { 2,6,10 };
-
-	std::list<std::string> rockQuestions_;
-	const std::unordered_set<int> rockPlaces_ = { 3,7,11 };
+	std::unordered_map<std::string, QuestionCategory> categories_ = {
+		{ "Pop", QuestionCategory("Pop", { 0,4,8 }) },
+		{ "Science", QuestionCategory("Science", { 1,5,9 })},
+		{ "Sports", QuestionCategory("Sports", { 2,6,10 })},
+		{ "Rock", QuestionCategory("Rock", { 3,7,11 })},
+	};
 };
 
 class PlaceNotFoundException : public std::exception
